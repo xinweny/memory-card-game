122b7e91bd1466e83b15784dbaee927c88055aad
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-02-02 12:09:36 -0500

    add cleanup for click event listener

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import '../styles/Card.css';
 
 function Card(props) {
 	const { name, imgUrl } = props.pokemon;
+
+	useEffect(() => {
+		return () => window.removeEventListener('click', props.handleClick);
+	});
 
 	return (
 		<div className="card" onClick={props.handleClick}>
