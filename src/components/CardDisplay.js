748d83e79a7ec1f302dc8b3819f5cc3a9a474aaa
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-02-02 15:50:27 -0500

    add styling

--- src/components/CardDisplay.js
+++ src/components/CardDisplay.js
@@ -7,12 +7,14 @@ import '../styles/CardDisplay.css';
 function CardDisplay(props) {
 	return (
 		<div className="card-display">
+			<div className="cards">
 			{props.pokemons.map(pokemon => 
 				<Card
 				key={pokemon.id} name={pokemon.name} url={pokemon.imgUrl}
 				handleClick={() => props.handleClick(pokemon.id)}
 				/>
 			)}
+			</div>
 		</div>
 	)
 }
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import '../styles/Scoreboard.css';
+
 function Scoreboard(props) {
 	return (
 		<div>
