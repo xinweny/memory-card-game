b401ed05680a893dc2020c9709071afff4a73292
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-02-03 11:47:11 -0500

    add loading screen animation

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import '../styles/LoadingScreen.css';
 
 function LoadingScreen(props) {
+	const [loadMsg, setLoadMsg] = useState('Loading...');
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setLoadMsg(prevMsg => {
+				const numDots = prevMsg.split('.').length - 1;
+
+				console.log(numDots);
+				return (numDots === 3) ? 'Loading' : prevMsg + '.';
+			});
+		}, 500);
+
+		return () => clearInterval(timer);
+	}, []);
+
 	return (
 		<div className="loading-screen">
 			<div className="loading-bar">
-				<p>Loading...</p>
+				<p>{loadMsg}</p>
 				<div className="bar">
 					<div className="progress" style={{ width: `${props.progress}%` }}>
 						<p>{Math.round(props.progress)}%</p>
