7180caaf47d9f961f80ba6a66266312c4ac51932
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-02-29 14:57:48 -0500

    move assets folder out of src

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -6,12 +6,18 @@ function StartScreen({ generations, setGeneration }) {
 	return (
 		<div className="start-modal pokemon-border">
       <div>
-        <h1>INSTRUCTIONS</h1>
-        <span>Select each Pokémon only ONCE per level. You will progress to the next level when all Pokémon have been selected once each. If you pick a Pokémon you already selected for that level, GAME OVER.</span>
+        <h3>INSTRUCTIONS</h3>
+        <span>
+          <span>Select each Pokémon only ONCE per level.</span>
+          <br />
+          <span><span className="text-positive">LEVEL UP</span> when you select all Pokémon for that level.</span>
+          <br />
+          <span><span className="text-danger">GAME OVER</span> when you select a Pokémon twice for that level.</span>
+        </span>
       </div>
       <div>
         <h2>SELECT GEN</h2>
-        <div>
+        <div className="gen-buttons">
           {generations && generations.map((gen, i) => (
             <button key={gen} onClick={() => { setGeneration(i + 1); }}>
               {gen}
